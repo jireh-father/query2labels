ce1d59f87dab4ab7297f079aa297533479d5ef8e
ilseo <ilseo@gmarket.com>
2024-02-22 10:26:37 +0900

    add fashion attr dataset

--- lib/dataset/get_dataset.py
+++ lib/dataset/get_dataset.py
@@ -89,7 +89,7 @@ def get_datasets(args):
             for file_name in train_label_dict:
                 tags = [v for v in train_label_dict[file_name]["tags"].split(", ") if v in tagger.val_to_key_map]
                 tag_set.update(tags)
-            if len(tags) != args.num_class:
+            if len(tag_set) != args.num_class:
                 random.shuffle(label_items)
                 num_train = int(len(label_items) * 0.9)
                 train_label_dict = dict(label_items[:num_train])
@@ -100,7 +100,7 @@ def get_datasets(args):
             for file_name in val_label_dict:
                 tags = [v for v in val_label_dict[file_name]["tags"].split(", ") if v in tagger.val_to_key_map]
                 tag_set.update(tags)
-            if len(tags) != args.num_class:
+            if len(tag_set) != args.num_class:
                 random.shuffle(label_items)
                 num_train = int(len(label_items) * 0.9)
                 train_label_dict = dict(label_items[:num_train])
